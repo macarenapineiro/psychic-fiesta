d978b97095bcbb540d08128d02408d80c442abdd
Macarenapineiro1 <macarena.pineiro@correo.ucu.edu.uy>
2024-08-13 10:16:46 -0300

    Merged suma

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,6 +1,8 @@
 ﻿Console.WriteLine(Addition.Add(7, 2));
 Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
+Console.WriteLine(Division.Divide(18, 2));
+
 public class Multiplication
 {
    public static int Multiply(int a, int b)
@@ -8,7 +10,6 @@ public class Multiplication
        return a * b;
    }
 }
-Console.WriteLine(Division.Divide(18, 2));
 
 // Esta clase implementa la operación Resta
 public class Addition
