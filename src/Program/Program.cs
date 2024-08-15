14a7bc34d778848506aba1272537f74caaaa8dd2
Macarenapineiro1 <macarena.pineiro@correo.ucu.edu.uy>
2024-08-15 11:12:02 -0300

    division

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -27,3 +27,11 @@ public class Substraction
         return a - b;
     }
 }
+// Esta clase implementa la operación division  
+public class Division
+{
+    public static double Divide(int a, int b)
+    {
+        return (double)a / b;
+    }
+}
